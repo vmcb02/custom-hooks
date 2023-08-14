61c44ae38769f7dd8c7c5ee6e2bdd7d7b790e201
acer@acer <vmcb0222@gmail.com>
2023-08-14 14:17:24 -0400

    Initial commit

---
+++ useCounter.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+
+
+export const useCounter = (initialValue) => {
+
+const [ counter, setCounter] = useState(initialValue)
+
+const increment = (value) => {
+    setCounter( counter + value);
+}
+
+const decrement = (value) => {
+    if( counter === 0) return;
+    setCounter( counter - value);
+}
+
+const reset = () => {
+    setCounter( initialValue );
+}
+
+  return {
+   counter,
+   increment,
+   decrement,
+   reset,
+  }
+}
